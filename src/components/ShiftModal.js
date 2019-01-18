c125acb5f8a34b539157408c6ce549b6fba29d10
Simon-Oliver <stauffer.simon@outlook.com>
2019-01-19 08:04:40 +1100

    updated modal

--- src/components/ShiftModal.js
+++ src/components/ShiftModal.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import Modal from 'react-modal';
 import '../styles/components/shiftModal.css';
+import { Button, Header, Icon, Modal, Form } from 'semantic-ui-react';
+
+const options = [
+  { key: 'm', text: 'Morning', value: 'MA', hours: 7 },
+  { key: 'e', text: 'Evening', value: 'E3' },
+  { key: 'n', text: 'Night', value: 'N1' },
+  { key: 'o', text: 'Off', value: 'DO' }
+];
 
 class ShiftModal extends React.Component {
   state = {
@@ -13,10 +20,10 @@ class ShiftModal extends React.Component {
     }
   }
 
-  selectedShift = e => {
+  selectedShift = (e, i) => {
     const newShift = {
       date: this.props.selectedShift.date,
-      shift: e.target.value,
+      shift: i.value,
       icon: 'brightness_2'
     };
 
@@ -26,40 +33,62 @@ class ShiftModal extends React.Component {
 
   render() {
     return (
-      <Modal
-        isOpen={!!this.props.selectedShift}
-        contentLabel="Selected Option"
-        onRequestClose={this.props.closeModal}
-        closeTimeoutMS={200}
-        className="modal"
-      >
-        <h3 className="modal__title">Selected Option</h3>
-        {this.state.selectedShift && (
-          <p className="modal__body">{this.state.selectedShift.shift}</p>
-        )}
-
-        <form>
-          <select
-            name="shift"
-            onChange={this.selectedShift}
-            defaultValue={
-              this.state.selectedShift.shift ? this.state.selectedShift.shift : 'default'
-            }
-          >
-            <option value="default" disabled>
-              Choose here
-            </option>
-            <option value="MA">Morning</option>
-            <option value="E3">Evening</option>
-            <option value="N1">Night</option>
-            <option value="DO">Off</option>
-          </select>
-        </form>
-
-        <button className="button" onClick={this.props.closeModal}>
-          Close
-        </button>
+      <Modal open={!!this.props.selectedShift} onClose={this.props.closeModal} basic size="mini">
+        <Modal.Header>
+          <Icon name="calendar" />
+          Select Shift
+        </Modal.Header>
+        <Modal.Content>
+          {this.state.selectedShift.shift && <h3>{this.state.selectedShift.shift}</h3>}
+          <Form>
+            <Form.Select
+              options={options}
+              placeholder="Select Shift"
+              onChange={this.selectedShift}
+            />
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="green" inverted onClick={this.props.closeModal}>
+            <Icon name="checkmark" /> Save
+          </Button>
+        </Modal.Actions>
       </Modal>
+
+      // <Modal
+      //   isOpen={}
+      //   contentLabel="Selected Option"
+      //   onRequestClose={this.props.closeModal}
+      //   closeTimeoutMS={200}
+      //   className="modal"
+      // >
+      //   <h3 className="modal__title">Selected Option</h3>
+      //   {this.state.selectedShift && (
+      //     <p className="modal__body">{this.state.selectedShift.shift}</p>
+      //   )}
+
+      //   <form>
+      //     <select
+      //       name="shift"
+      //       onChange={this.selectedShift}
+      //       defaultValue={
+      //         this.state.selectedShift.shift ? this.state.selectedShift.shift : 'default'
+      //       }
+      //     >
+      //       <option value="default" disabled>
+      //         Choose here
+      //       </option>
+      //       <option value="MA">Morning</option>
+      //       <option value="E3">Evening</option>
+      //       <option value="N1">Night</option>
+      //       <option value="DO">Off</option>
+      //     </select>
+      //   </form>
+
+      //   <button className="button" onClick={this.props.closeModal}>
+      //     Close
+      //   </button>
+      // </Modal>
     );
   }
 }
